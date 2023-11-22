0e120559f9433e8826f9eba5b890893100e2f1e2
odedstr <dodothedremer@gmail.com>
2023-11-22 22:41:45 +0200

    Made the captions draggble

--- src/content.js
+++ src/content.js
@@ -1,3 +1,30 @@
+// Function to make a div draggable
+function makeDivDraggable(div) {
+	var startY, startTop;
+
+	// Function to handle the start of dragging
+	function onMouseDown(e) {
+		startY = e.clientY;
+		startTop = parseInt(window.getComputedStyle(div).top, 10);
+
+		document.addEventListener('mousemove', onMouseMove);
+		document.addEventListener('mouseup', onMouseUp);
+	}
+
+	// Function to handle the mouse movement
+	function onMouseMove(e) {
+		var newY = startTop + e.clientY - startY;
+		div.style.top = newY + 'px';
+	}
+
+	// Function to handle the end of dragging
+	function onMouseUp() {
+		document.removeEventListener('mousemove', onMouseMove);
+		document.removeEventListener('mouseup', onMouseUp);
+	}
+
+	div.addEventListener('mousedown', onMouseDown);
+}
 
 document.querySelector('[aria-label="Show transcript"]').click();
 
@@ -6,10 +33,13 @@ captions_container.classList.add("youtube-full-captions-container");
 captions_container.innerHTML = "<div class='youtube-full-captions-text' style=''><div>";
 let player_element = document.querySelector("#player");
 player_element.appendChild(captions_container);
+makeDivDraggable(captions_container);
 
 const fullscreen_captions_container = captions_container.cloneNode(true);
 let fullscreen_player_element = document.querySelector("#player-full-bleed-container");
 fullscreen_player_element.appendChild(fullscreen_captions_container);
+makeDivDraggable(fullscreen_captions_container);
+
 
 const youtube_full_captions_text_elements = document.querySelectorAll('.youtube-full-captions-container .youtube-full-captions-text');
 
@@ -48,35 +78,3 @@ const checkParentElement = setInterval(() => {
 	}
 }, 100); // checks every 500 milliseconds
 
-
-
-
-
-
-	// if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-  //   // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
-  //   const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-  //   // Next state will always be the opposite
-  //   const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-  //
-  //   // Set the action badge to the next state
-  //   await chrome.action.setBadgeText({
-  //     tabId: tab.id,
-  //     text: nextState
-  //   });
-  //
-  //   if (nextState === 'ON') {
-  //     // Insert the CSS file when the user turns the extension on
-  //     await chrome.scripting.insertCSS({
-  //       files: ['focus-mode.css'],
-  //       target: { tabId: tab.id }
-  //     });
-  //   } else if (nextState === 'OFF') {
-  //     // Remove the CSS file when the user turns the extension off
-  //     await chrome.scripting.removeCSS({
-  //       files: ['focus-mode.css'],
-  //       target: { tabId: tab.id }
-  //     });
-  //   }
-  // }
-// });
