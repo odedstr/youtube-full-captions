a64a1de58390b9d29c1c65cb66c1db84c58f43c8
odedstr <dodothedremer@gmail.com>
2023-11-24 04:29:43 +0200

    Added option to turn the captions off and on again. The on again doesn't work properly yet.

--- src/background.js
+++ src/background.js
@@ -1,12 +1,59 @@
-chrome.action.onClicked.addListener((tab) => {
+let script_inserted = false;
+let full_captions_active = false;
 
-	chrome.scripting.insertCSS({
-		target: {tabId: tab.id},
-		files: ['content.css']
-	});
+// chrome.runtime.onInstalled.addListener(async () => {
+// 	await chrome.action.setBadgeText({
+// 		text: 'OFF'
+// 	});
+// });
 
-	chrome.scripting.executeScript({
-		target: {tabId: tab.id},
-		files: ['content.js']
-	});
+chrome.action.onClicked.addListener(async (tab) => {
+
+	// // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
+	// const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+	// // Next state will always be the opposite
+	// const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+	//
+	// // Set the action badge to the next state
+	// await chrome.action.setBadgeText({
+	// 	tabId: tab.id,
+	// 	text: nextState
+	// });
+
+	if(!script_inserted){
+		script_inserted = true;
+
+		await chrome.scripting.executeScript({
+			target: {tabId: tab.id},
+			files: ['content.js']
+		});
+	}
+
+	if(!full_captions_active){
+		full_captions_active = true;
+
+		await chrome.scripting.insertCSS({
+			target: {tabId: tab.id},
+			files: ['content.css']
+		});
+
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {message: "turnOn"});
+		});
+	}
+	else{
+		// Remove the CSS
+		await chrome.scripting.removeCSS({
+			target: {tabId: tab.id},
+			files: ['content.css']
+		});
+
+		// Cancel everything that content.js does
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {message: "turnOff"});
+		});
+		full_captions_active = false;
+	}
+
+
 });
--- src/content.js
+++ src/content.js
@@ -1,3 +1,14 @@
+chrome.runtime.onMessage.addListener(
+	async function (request, sender, sendResponse) {
+		if (request.message === "turnOn") {
+			await turnOn();
+			// console.debug("In content script");
+		} else if (request.message === "turnOff") {
+			await turnOff();
+		}
+	}
+);
+
 function waitForElement(selector, timeout = 30000) {
 	return new Promise((resolve, reject) => {
 		const intervalTime = 100; // Interval check time in milliseconds
@@ -137,8 +148,39 @@ function redirectClickEventOnElement(element) {
 	});
 }
 
+let resizeObserver = null;
+let fullscreenResizeObserver = null;
+let waitForActiveClassObserver = null;
+let captions_text_element_stop_monitor_positison = null;
 
-(async () => {
+async function turnOff() {
+
+	const caption_elements = document.querySelectorAll(".youtube-full-captions-container")
+	caption_elements.forEach(element => {
+		element.remove();
+	});
+
+	if(resizeObserver != null){
+		resizeObserver.disconnect();
+	}
+
+	if(captions_text_element_stop_monitor_positison != null){
+		captions_text_element_stop_monitor_positison();
+	}
+
+	let fullscreen_player_element = document.querySelector("#player-full-bleed-container");
+	fullscreen_player_element.classList.remove('youtube-full-captions-container-fullscreen');
+
+	if(fullscreenResizeObserver != null){
+		fullscreenResizeObserver.disconnect();
+	}
+
+	if(waitForActiveClassObserver != null){
+		waitForActiveClassObserver.disconnect();
+	}
+}
+
+async function turnOn() {
 
 	const youtube_cc_button_selector = 'button.ytp-subtitles-button[aria-pressed="false"]';
 	await waitForElement(youtube_cc_button_selector, -1);
@@ -156,32 +198,29 @@ function redirectClickEventOnElement(element) {
 	await waitForElement(".caption-window.ytp-caption-window-bottom", -1);
 
 	let player_element = document.querySelector("#player");
-	console.debug("player_element", player_element);
 	player_element.appendChild(captions_container);
-	console.debug("After player_element.appendChild. captions_container:", captions_container);
 	const captions_text_element = captions_container.querySelector(".youtube-full-captions-text");
 
 	redirectClickEventOnElement(captions_container);
 
 	makeDivDraggable(captions_container);
 
-	monitorElementPosition(captions_text_element,
+	captions_text_element_stop_monitor_positison = monitorElementPosition(captions_text_element,
 		player_element,
-		function(element){
+		function (element) {
 			element.classList.add("outside-container");
 		},
-		function(element){
+		function (element) {
 			element.classList.remove("outside-container");
 		});
 
-	var resizeObserver = new ResizeObserver(function(entries) {
+	resizeObserver = new ResizeObserver(function (entries) {
 		// For all entries (there should only be one in this case)
 		for (let entry of entries) {
 			adjustFontSize(entry, 3, captions_text_element, 13.71, 27.35);
 		}
 	});
 	resizeObserver.observe(player_element);
-
 
 
 	const fullscreen_captions_container = captions_container.cloneNode(true);
@@ -192,7 +231,7 @@ function redirectClickEventOnElement(element) {
 
 	makeDivDraggable(fullscreen_captions_container);
 
-	var fullscreenResizeObserver = new ResizeObserver(function(entries) {
+	fullscreenResizeObserver = new ResizeObserver(function (entries) {
 		// For all entries (there should only be one in this case)
 		for (let entry of entries) {
 			adjustFontSize(entry, 3, fullscreen_captions_text_element, 13.71, 27.35);
@@ -206,40 +245,26 @@ function redirectClickEventOnElement(element) {
 	function copyContents() {
 		const activeElement = document.querySelector('.ytd-transcript-segment-list-renderer.active');
 		if (activeElement) {
-			youtube_full_captions_text_elements.forEach(function(element) {
+			youtube_full_captions_text_elements.forEach(function (element) {
 				element.innerHTML = activeElement.querySelector("yt-formatted-string").innerHTML;
 			});
 		}
 	}
 
-	function setupObserver() {
-		const observer = new MutationObserver((mutations, obs) => {
-			for (let mutation of mutations) {
-				if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-					copyContents();
-				}
+	await waitForElement('#segments-container.ytd-transcript-segment-list-renderer', -1);
+
+	waitForActiveClassObserver = new MutationObserver((mutations, obs) => {
+		for (let mutation of mutations) {
+			if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+				copyContents();
 			}
-		});
-
-		const config = { attributes: true, childList: true, subtree: true };
-		const parentElement = document.querySelector('#segments-container.ytd-transcript-segment-list-renderer');
-		observer.observe(parentElement, config);
-
-		// Optional: To stop observing at some point
-		// observer.disconnect();
-	}
-
-// Polling for the existence of parentElement
-	const checkParentElement = setInterval(() => {
-		const parentElementExists = document.querySelector('#segments-container.ytd-transcript-segment-list-renderer');
-		if (parentElementExists) {
-			clearInterval(checkParentElement);
-			setupObserver();
 		}
-	}, 100); // checks every 500 milliseconds
+	});
 
+	const config = {attributes: true, childList: true, subtree: true};
+	const parentElement = document.querySelector('#segments-container.ytd-transcript-segment-list-renderer');
+	waitForActiveClassObserver.observe(parentElement, config);
 
-})();
-
+}
 
 
